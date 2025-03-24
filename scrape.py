d2215668390a3ffa935e4319f4d1d9444d315ff8
Akshay Sinha <e22cseu0385@bennett.edu.in>
2025-03-24 19:55:38 +0530

    defining the scrapping function using selenium

--- scrape.py
+++ scrape.py
@@ -1,3 +1,18 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 
+def scrape_website (website):
+    print(f"Launching Chrome browser...")
+
+    chrome_driver_path = ""
+    options = webdriver.ChromeOptions() 
+    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+
+    try:
+        driver.get(website)
+        print("Page Loaded...")
+        html = driver.page_source
+
+        return html
+    finally:
+        driver.quit()
