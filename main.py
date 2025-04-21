0e166004f088ecc32c232a5a1375a69b0196e5b6
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-04-21 19:19:57 +0530

    used brightdata to bypass captcha

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ url = st.text_input('Enter the URL of the website you want to scrape')
 if st.button("Scrape Site"):
     st.write(f"Scraping {url}...")
     result = scrape_website(url)
+    print(result)
 
--- scrape.py
+++ scrape.py
@@ -1,20 +1,28 @@
-import selenium.webdriver as webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 import time
 
+AUTH = 'brd-customer-hl_140e2801-zone-ai_web_scrapper:t5d3h2186wu0'
+SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
+
 def scrape_website (website):
-    print(f"Launching Chrome browser...")
+    print("Launching Chrome Browser...")
 
-    chrome_driver_path = "./chromedriver"
-    options = webdriver.ChromeOptions() 
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-
-    try:
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, "goog", "chrome")
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
         driver.get(website)
-        print("Page Loaded...")
+        #CAPTCHA SOLVER 
+        print("Waiting captcha to solve...")
+        solve_res = driver.execute(
+            "executeCdpCommand",
+            {
+                "cmd": "Captcha.waitForSolve",
+                "params": {"detectTimeout": 10000},
+            },
+        )
+        print("Captcha solve status:", solve_res["value"]["status"])
+        print("Navigated! Scraping page content...")
         html = driver.page_source
-        time.sleep(10)
+        return html 
+    
 
-        return html
-    finally:
-        driver.quit()
