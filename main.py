e56077a313dc7e8ded3dc3ed7a23f8f3099161b0
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-04-21 19:45:49 +0530

    cleaning DOM Content

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import (
+    scrape_website, 
+    spilt_dom_content, 
+    clean_body_content, 
+    extract_body_content,
+)
 
 st.title('Scrape the web with AI')
 url = st.text_input('Enter the URL of the website you want to scrape')
@@ -7,5 +12,5 @@ url = st.text_input('Enter the URL of the website you want to scrape')
 if st.button("Scrape Site"):
     st.write(f"Scraping {url}...")
     result = scrape_website(url)
-    print(result)
+    
 
--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 from selenium.webdriver import Remote, ChromeOptions
 from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
-import time
+from bs4 import BeautifulSoup
 
 AUTH = 'brd-customer-hl_140e2801-zone-ai_web_scrapper:t5d3h2186wu0'
 SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
@@ -23,6 +23,32 @@ def scrape_website (website):
         print("Captcha solve status:", solve_res["value"]["status"])
         print("Navigated! Scraping page content...")
         html = driver.page_source
-        return html 
+        return html
+
+# Cleaning the DOM Content
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    body_content = soup.body
+    if body_content:
+         return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, 'html.parser')
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
+
+def spilt_dom_content(dom_content, max_length=6000): 
+    return [
+        dom_content[i: i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
     
 
